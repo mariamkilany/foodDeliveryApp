94702dfb69427bdac1b491afb3d4d6f88c76b79b
yasmin <20191815@fue.edu.eg>
2024-02-28 19:27:41 +0200

    navbar and meal details

--- src/app/Components/Menu Components/dish/dish.component.ts
+++ src/app/Components/Menu Components/dish/dish.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dish',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './dish.component.html',
   styleUrl: './dish.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,16 +4,18 @@ import { DishesPageComponent } from './Pages/dishes-page/dishes-page.component';
 import { AboutPageComponent } from './Pages/about-page/about-page.component';
 import { ContactPageComponent } from './Pages/contact-page/contact-page.component';
 import { SigninPageComponent } from './Pages/signin-page/signin-page.component';
+import { DetailsComponent } from './Pages/details/details.component';
 
 export const routes: Routes = [
-    
-    {path:"" ,component:LandingPageComponent},
-    {path:"about" ,component:AboutPageComponent},
-    {path:"menu" ,component:DishesPageComponent},
-    {path:"contact" ,component:ContactPageComponent},
-    {path:"sign" ,component:SigninPageComponent},
-
-
-
-
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  { path: 'home', component: LandingPageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'menu', component: DishesPageComponent },
+  { path: 'details', component: DetailsComponent },
+  { path: 'contact', component: ContactPageComponent },
+  { path: 'sign', component: SigninPageComponent },
 ];
