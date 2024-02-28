4f9a2b4da737ca8e5d7da50ebe57fa50604cc867
mariamkilany <mahmoud32043@gmail.com>
2024-02-29 00:04:44 +0200

    Get and add Reviews

--- src/app/Pages/details/details.component.ts
+++ src/app/Pages/details/details.component.ts
@@ -2,15 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { ProductDetailsComponent } from '../../Components/product-details/product-details.component';
 import { MealsService } from '../../Services/meals.service';
 import { ActivatedRoute } from '@angular/router';
+import { ReviewsComponent } from '../../Components/reviews/reviews.component';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [ProductDetailsComponent],
+  imports: [ProductDetailsComponent,ReviewsComponent],
   providers: [MealsService],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css',
 })
 export class DetailsComponent  {
- 
+
 }
