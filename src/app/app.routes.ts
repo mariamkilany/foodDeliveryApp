e814a1c814a91829477d5158f3bf071cec043af6
mariam-eltayeb <mariam.eltayeb@outlook.com>
2024-02-28 20:55:27 +0200

    sign in with google and guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { DishesPageComponent } from './Pages/dishes-page/dishes-page.component';
 import { AboutPageComponent } from './Pages/about-page/about-page.component';
 import { ContactPageComponent } from './Pages/contact-page/contact-page.component';
 import { SigninPageComponent } from './Pages/signin-page/signin-page.component';
+import { authGuard } from './Services/auth.guard';
 
 export const routes: Routes = [
     
     {path:"" ,component:LandingPageComponent},
     {path:"about" ,component:AboutPageComponent},
-    {path:"menu" ,component:DishesPageComponent},
+    {path:"menu" ,component:DishesPageComponent, canActivate:[authGuard]},
     {path:"contact" ,component:ContactPageComponent},
     {path:"sign" ,component:SigninPageComponent},
 
