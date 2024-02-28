801a9134d9bf98745a4c979b82836e0b32ad5f1c
mariamkilany <mahmoud32043@gmail.com>
2024-02-28 20:05:47 +0200

    Reviews Desgin

---
+++ src/app/Components/review/review.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './review.component.html',
+  styleUrl: './review.component.css',
+})
+export class ReviewComponent {}
