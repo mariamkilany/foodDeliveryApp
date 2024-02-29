69d7f1d1f36a9df593d88af85d8dbe04fb69b6ae
Youssef Adly <youssefadly404@gmail.com>
2024-02-29 13:02:59 +0200

    Cart And Contact Us

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,16 +4,13 @@ import { DishesPageComponent } from './Pages/dishes-page/dishes-page.component';
 import { AboutPageComponent } from './Pages/about-page/about-page.component';
 import { ContactPageComponent } from './Pages/contact-page/contact-page.component';
 import { SigninPageComponent } from './Pages/signin-page/signin-page.component';
+import { CartComponent } from './Pages/cart/cart.component';
 
 export const routes: Routes = [
-    
-    {path:"" ,component:LandingPageComponent},
-    {path:"about" ,component:AboutPageComponent},
-    {path:"menu" ,component:DishesPageComponent},
-    {path:"contact" ,component:ContactPageComponent},
-    {path:"sign" ,component:SigninPageComponent},
-
-
-
-
+  { path: '', component: LandingPageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'menu', component: DishesPageComponent },
+  { path: 'contact', component: ContactPageComponent },
+  { path: 'sign', component: SigninPageComponent },
+  { path: 'cart', component: CartComponent },
 ];
