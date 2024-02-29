c6167eeed7e0a560d1bfc52465f60ee2ca5dea87
Gameel Allam <fadyallam8@gmail.com>
2024-02-29 19:56:15 +0200

    update random dishes components

--- src/app/Components/LandingPage Components/land-dish-card/land-dish-card.component.ts
+++ src/app/Components/LandingPage Components/land-dish-card/land-dish-card.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MealsService } from '../../../Services/meals.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-land-dish-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
+  providers: [MealsService],
   templateUrl: './land-dish-card.component.html',
-  styleUrl: './land-dish-card.component.css'
+  styleUrl: './land-dish-card.component.css',
 })
-export class LandDishCardComponent {
+export class LandDishCardComponent implements OnInit {
+  meal1: any;
+  meal2: any;
+  meal3: any;
+  constructor(private mealsService: MealsService) {}
 
+  ngOnInit() {
+    this.mealsService.getRandomMeal().subscribe((data: any) => {
+      this.meal1 = data.meals[0];
+      this.meal1.name = this.meal1.strMeal.slice(0, 16);
+    });
+    this.mealsService.getRandomMeal().subscribe((data: any) => {
+      this.meal2 = data.meals[0];
+      this.meal2.name = this.meal2.strMeal.slice(0, 16);
+    });
+    this.mealsService.getRandomMeal().subscribe((data: any) => {
+      this.meal3 = data.meals[0];
+      this.meal3.name = this.meal3.strMeal.slice(0, 16);
+    });
+  }
 }
