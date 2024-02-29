df023e5259c80aa245bc5f143e8304c723fa6ac3
Youssef Adly <youssefadly404@gmail.com>
2024-02-29 17:09:28 +0200

    Fix Cart

--- src/app/Pages/cart/cart.component.ts
+++ src/app/Pages/cart/cart.component.ts
@@ -3,34 +3,37 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { CardService } from '../../Services/card.service';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
   imports: [CommonModule, FormsModule, HttpClientModule, RouterModule],
+  providers: [CardService],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
 export class CartComponent implements OnInit {
+  user: any;
   cart: any;
 
-  // { name: string; price: number; quantity: number }[] = [
-  //   { name: 'Fried Chickens', price: 2.99, quantity: 1 },
-  //   { name: 'Pizza', price: 2.99, quantity: 1 },
-  //   { name: 'Cakes', price: 2.99, quantity: 1 },
-  // ];
-
-  constructor(private http: HttpClient) {}
-
-  getCart() {
-    return this.http.get(
-      'https://65def281ff5e305f32a0f1bd.mockapi.io/cartItem?userEmail=fady@gmail.com&completed=false'
-    );
+  constructor(private http: HttpClient, private cartService: CardService) {
+    this.user = JSON.parse(sessionStorage.getItem('loggedInUser') || '');
   }
 
+  // getCart() {
+  //   return this.http.get(
+  //     'https://65def281ff5e305f32a0f1bd.mockapi.io/cartItem?userEmail=fady@gmail.com&completed=false'
+  //   );
+  // }
+
   ngOnInit() {
-    this.getCart().subscribe((data) => {
-      console.log(data);
+    // this.getCart().subscribe((data) => {
+    //   console.log(data);
+    //   this.cart = data;
+    // });
+    this.cartService.getCard(this.user.email).subscribe((data) => {
+      console.log('data: ', data);
       this.cart = data;
     });
   }
@@ -47,13 +50,16 @@ export class CartComponent implements OnInit {
     }
   }
 
-  removeItem(index: number) {
+  removeItem(index: number, id: number) {
     this.cart.splice(index, 1);
+    this.cartService.removeFromCart(id).subscribe((data) => {
+      console.log(data);
+    });
   }
 
   get totalPrice() {
     let totalPrice = 0;
-    for (let i = 0; i < this.cart.length; i++) {
+    for (let i = 0; i < this.cart?.length; i++) {
       const p = this.cart[i];
       totalPrice += p.price * p.quantity;
     }
